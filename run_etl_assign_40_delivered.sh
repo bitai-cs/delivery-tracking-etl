37e426fc666179406b7f2bee65639808735996f7
Viko Bastidas <vico.bastidas@gmail.com>
2025-04-29 12:06:21 -0500

    New 40 (delivered) status

---
+++ run_etl_assign_40_delivered.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd /opt/delivery-tracking-etl
+# Calcular la fecha de ayer
+YESTERDAY_LOG="cron_etl_assign_40_delivered_$(date -d "yesterday" +'%Y%m%d').log"
+# Eliminar el archivo log de ayer si existe
+if [ -f "$YESTERDAY_LOG" ]; then
+    rm "$YESTERDAY_LOG"
+    echo "Deleted previous day log file: $YESTERDAY_LOG"
+fi
+# Crear el nuevo archivo log para hoy
+LOG_FILE="cron_etl_assign_40_delivered_$(date +'%Y%m%d').log"
+echo "Executing .sh file -> DT: $(date)" >> /opt/delivery-tracking-etl/$LOG_FILE
+# Tomar en cuenta que en la instalación del proyecto ETLs se debió
+# correr el comando "poetry install" para crear el entorno virtual python
+# e instalar las dependencias del proyecto
+poetry run etl_assign_40_delivered >> /opt/delivery-tracking-etl/$LOG_FILE 2>&1
